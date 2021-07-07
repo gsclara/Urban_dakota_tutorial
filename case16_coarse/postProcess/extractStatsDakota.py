ca74189362ca055f1e47d4a46087510d23057dc7
gsclara <C.Garcia-Sanchez@tudelft.nl>
2021-07-07 10:46:27 +0200

    extractStatsDakota.py fixed surfaces source

--- case16_coarse/postProcess/extractStatsDakota.py
+++ case16_coarse/postProcess/extractStatsDakota.py
@@ -21,7 +21,7 @@ outputStats = [Umean, Uvar, Uskew, Ukurt]
 outputStatsName = ['Umean', 'Uvar', 'Uskew', 'Ukurt']
 c=0
 for iE in outputStats: 
-    grid_new = pyvista.PolyData('U_zNormal.vtk')
+    grid_new = pyvista.PolyData('workdir.1/postProcessing/surfaces/3000/U_zNormal.vtk')
     grid_new.point_arrays['U'] = iE  
     grid_new.save(outputStatsName[c]+'.vtk', binary=False) 
     c=c+1
